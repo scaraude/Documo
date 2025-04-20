d9c537c8cca6210bcfdfbe5e42e947d330ebe978
scaraude <ludodante@hotmail.fr>
2025-04-20 19:55:15 -0300

    add redirect on the logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,9 +5,9 @@ export const Navbar = () => {
         <nav className="bg-white shadow-md">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
-                    <div className="flex items-center">
+                    <Link href="/" className="flex items-center">
                         <span className="text-xl font-bold text-blue-600">DocumentTransfer</span>
-                    </div>
+                    </Link>
 
                     <div className="flex items-center space-x-8">
                         <Link href="/"
@@ -27,6 +27,6 @@ export const Navbar = () => {
                     </div>
                 </div>
             </div>
-        </nav>
+        </nav >
     )
 }
