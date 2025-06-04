31a91530ff57a613828b735048ecbd7c31b6a55f
scaraude <ludodante@hotmail.fr>
2025-06-04 00:57:32 -0300

    refactor: remove FormCreateRequest and RequestsList components from exports

--- features/requests/components/index.ts
+++ features/requests/components/index.ts
@@ -1,6 +1,3 @@
-// features/requests/components/index.ts
-export { FormCreateRequest } from './FormCreateRequest';
-export { RequestsList } from './RequestsList';
 export { RequestAccordion } from './RequestAccordion';
 export { RequestFilters } from './RequestFilters';
 export { RequestSearchAndSort } from './RequestSearchAndSort';
